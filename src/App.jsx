bb07cdd644827b51b6ed846708f720d9854f2351
suswin <suswinsuswin23@gmail.com>
2026-05-14 10:43:58 +0530

    fix basename for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useDarkMode } from './hooks/useDarkMode'
-import { useKeyboardShortcuts } from './hooks/useKeyboardShortcuts'
-import Home from './pages/Home'
-import UserProfile from './pages/UserProfile'
-import Compare from './pages/Compare'
-import Leaderboard from './pages/Leaderboard'
-import NotFound from './pages/NotFound'
-import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDarkMode } from "./hooks/useDarkMode";
+import { useKeyboardShortcuts } from "./hooks/useKeyboardShortcuts";
+import Home from "./pages/Home";
+import UserProfile from "./pages/UserProfile";
+import Compare from "./pages/Compare";
+import Leaderboard from "./pages/Leaderboard";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
 
 function AppInner({ dark, setDark }) {
-  useKeyboardShortcuts({ setDark })
+  useKeyboardShortcuts({ setDark });
   return (
     <>
       <Navbar dark={dark} setDark={setDark} />
@@ -21,14 +21,14 @@ function AppInner({ dark, setDark }) {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
 
 export default function App() {
-  const [dark, setDark] = useDarkMode()
+  const [dark, setDark] = useDarkMode();
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/github-viewer">
       <AppInner dark={dark} setDark={setDark} />
     </BrowserRouter>
-  )
+  );
 }
